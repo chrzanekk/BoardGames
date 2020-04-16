1c24faa3c44ac01356a048fae82d1f9964e78df5
Konrad <chrzanekk@gmail.com>
2020-04-16 17:52:23 +0200

    a few steps forward.

--- src/pl/com/konrad/checkers/CheckersPlayer.java
+++ src/pl/com/konrad/checkers/CheckersPlayer.java
@@ -1,6 +1,9 @@
 package pl.com.konrad.checkers;
 
+import java.util.ArrayList;
+
 public class CheckersPlayer extends Player{
+    private static final ArrayList<Figure> figureSet = new ArrayList<Figure>();
     private char playerPawn;
 
     public CheckersPlayer(String name, char playerPawn) {
