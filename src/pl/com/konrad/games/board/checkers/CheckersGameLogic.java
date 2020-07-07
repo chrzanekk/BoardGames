b0492dcfebc554502c58a02cbedc5f1081cd7c3e
Konrad <chrzanekk@gmail.com>
2020-07-07 11:25:12 +0200

    added prohibited fields in checkers board.

--- src/pl/com/konrad/games/board/checkers/CheckersGameLogic.java
+++ src/pl/com/konrad/games/board/checkers/CheckersGameLogic.java
@@ -1,9 +1,10 @@
 package pl.com.konrad.games.board.checkers;
 
 
+
+import pl.com.konrad.games.board.GameBoard;
 import pl.com.konrad.games.board.Player;
 
-import java.util.ArrayList;
 import java.util.List;
 
 //lista figur w logice + operacje na figurach(sprawdzanie itp)
@@ -32,18 +33,18 @@ public class CheckersGameLogic {
         return false;
     }
 
-//    przemyśleć czy metoda poniżej ma w ogóle sens.
+    //    przemyśleć czy metoda poniżej ma w ogóle sens.
     boolean isPlayerCanMovePawn(Player currentPlayer, Player playerOne, Player playerTwo, int row, int col) {
-        return isPlayerCanMovePawnBottom(currentPlayer,playerOne,row,col) || isPlayerCanMovePawnTop(currentPlayer,
-                playerTwo,row,col);
+        return isPlayerCanMovePawnBottom(currentPlayer, playerOne, row, col) || isPlayerCanMovePawnTop(currentPlayer,
+                playerTwo, row, col);
     }
 
     boolean isPlayerCanMovePawnBottom(Player currentPlayer, Player playerOne, int row, int col) {
-        return currentPlayer.equals(playerOne) && checkForPlaceToMoveBottom(row,col);
+        return currentPlayer.equals(playerOne) && checkForPlaceToMoveBottom(row, col);
     }
 
     boolean isPlayerCanMovePawnTop(Player currentPlayer, Player playerTwo, int row, int col) {
-        return currentPlayer.equals(playerTwo) && checkForPlaceToMoveTop(row,col);
+        return currentPlayer.equals(playerTwo) && checkForPlaceToMoveTop(row, col);
     }
 
     boolean checkForPlaceToMoveBottom(int userRow, int userCol) {
@@ -70,10 +71,25 @@ public class CheckersGameLogic {
         }
     }
 
-    void changeFigureType(int userRow, int userCol) {
-
+    void changeFigureType(int userRow, int userCol, GameBoard gameBoard, Player currentPlayer) {
+        if (userRow == gameBoard.getLength()) {
+            figures.remove(getFigureByRowCol(userRow,userCol));
+//            figures.add(new CheckersFigure(currentPlayer.getColor(),userRow,userCol,currentPlayer,));
+//dopisac metody pobierajace pola z figury po koordynatach.
+        }
     }
+
+    CheckersFigure getFigureByRowCol(int row, int col) {
+        for (CheckersFigure figure : figures) {
+            if (figure.getCurrentRow() == row && figure.getCurrentCol() == col) {
+                return new CheckersFigure(figure.getColor(), row, col, figure.getPlayer(), figure.getType(), figure.getMark());
+            }
+        }
+        return null;
+    }
+
 }
+
 
 
 
--- src/pl/com/konrad/games/board/checkers/CheckersPlayerLogic.java
+++ src/pl/com/konrad/games/board/checkers/CheckersPlayerLogic.java
@@ -1,6 +1,6 @@
 package pl.com.konrad.games.board.checkers;
 
-import pl.com.konrad.games.board.Player;
+
 
 import java.util.List;
 
@@ -23,17 +23,5 @@ class CheckersPlayerLogic {
         return false;
     }
 
-    static boolean isFigureBelongToPlayer(List<CheckersFigure> figures, int row, int col,
-                                          String currentPlayerName) {
-        for (CheckersFigure figure : figures) {
-            if (currentPlayerName.equals(figure.getPlayer().getName()) && figure.getCurrentRow() == row && figure.getCurrentCol() == col)
-                return true;
-        }
-        return false;
-    }
-
-
-
-   
 
 }
